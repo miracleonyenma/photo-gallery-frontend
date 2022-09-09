e6d9094f28ca5b56792a4c6bd42acce9cf79c168
miracleonyenma <miracleficient@gmail.com>
2022-09-09 06:08:02 +0100

    init: initial commit, set up Nuxt 3 with tailwind

---
+++ app.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1 class=" text-5xl">What?</h1>
+  </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,19 @@
+import { defineNuxtConfig } from 'nuxt'
+
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
+export default defineNuxtConfig({
+  build: {
+    postcss: {
+      postcssOptions: {
+        plugins: {
+          tailwindcss: {},
+          autoprefixer: {},
+        },
+      },
+    },
+  },
+
+  css: [
+    '@/assets/css/main.css',
+  ],
+})
